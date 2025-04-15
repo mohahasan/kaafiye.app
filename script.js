a594e654470ca34601e383d376cbc0108a355501
mohahasan <45252502+mohahasan@users.noreply.github.com>
2025-04-15 17:48:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ function toggleMenu() {
   }
   
   function downloadApp() {
-    window.location.href = "https://shorturl.at/6mGqM"; // Replace with real link
+      window.location.href = "https://play.google.com/store/apps/details?id=so.kaafiye.app"; 
   }
   
