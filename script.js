df94ac276833948988092a31b7d120b8c11278d9
mohahasan <45252502+mohahasan@users.noreply.github.com>
2025-04-15 17:42:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    document.querySelector("nav ul").classList.toggle("active");
+  }
+  
+  function closeMenu() {
+    document.querySelector("nav ul").classList.remove("active");
+  }
+  
+  function downloadApp() {
+    window.location.href = "https://shorturl.at/6mGqM"; // Replace with real link
+  }
+  
